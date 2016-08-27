c19ff39a4f84879994c1c6e3fa0a46080d82bf66
Bijan <bijanbwb@gmail.com>
2016-08-27 15:15:11 -0400

    Create and apply demo seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Hello World", liveDemoUrl: "#", sourceCodeUrl: "#"})
+ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Counter", liveDemoUrl: "#", sourceCodeUrl: "#"})
+ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Mario", liveDemoUrl: "#", sourceCodeUrl: "#"})
