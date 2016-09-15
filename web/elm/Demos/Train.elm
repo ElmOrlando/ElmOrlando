0e3a3e2ec4fd3475c6c5e0ae6fd9c5566d754f67
Bijan <bijanbwb@gmail.com>
2016-09-15 12:26:45 -0500

    Add initial versions of minigame demos

---
+++ web/elm/Demos/Train.elm
@@ -0,0 +1,133 @@
+-- Train Rolls Into Station
+-- "My favorite part was when the train rolled into the station."
+
+
+module Main exposing (..)
+
+import Html.App as App
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Keyboard exposing (KeyCode)
+import Key exposing (..)
+
+
+-- MAIN
+
+
+main : Program Never
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 50, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = KeyDown KeyCode
+    | KeyUp KeyCode
+
+
+update : Msg -> Model -> ( Int, Cmd Msg )
+update msg model =
+    case msg of
+        KeyDown keyCode ->
+            (keyDown keyCode model)
+
+        KeyUp keyCode ->
+            (keyUp keyCode model)
+
+
+keyDown : KeyCode -> Model -> ( Int, Cmd Msg )
+keyDown keyCode model =
+    case Key.fromCode keyCode of
+        ArrowLeft ->
+            ( model - 20, Cmd.none )
+
+        ArrowRight ->
+            ( model + 20, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+keyUp : KeyCode -> Model -> ( Int, Cmd Msg )
+keyUp keyCode model =
+    case Key.fromCode keyCode of
+        ArrowLeft ->
+            ( model + 0, Cmd.none )
+
+        ArrowRight ->
+            ( model + 0, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        position =
+            toString model
+    in
+        Svg.svg
+            [ Svg.Attributes.version "1.1"
+            , Svg.Attributes.width "840"
+            , Svg.Attributes.height "525"
+            , Svg.Attributes.viewBox ("0 0 840 525")
+            , Html.Attributes.style [ ( "background-color", "black" ) ]
+            ]
+            [ Svg.rect
+                [ Svg.Attributes.class "ground"
+                , Svg.Attributes.fill "lightgray"
+                , Svg.Attributes.x "0"
+                , Svg.Attributes.y "425"
+                , Svg.Attributes.width "840"
+                , Svg.Attributes.height "100"
+                ]
+                []
+            , Svg.image
+                [ Svg.Attributes.xlinkHref "img/train.png"
+                , Svg.Attributes.x position
+                , Svg.Attributes.y "335"
+                , Svg.Attributes.width "100"
+                , Svg.Attributes.height "100"
+                ]
+                []
+            ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Keyboard.downs KeyDown
+        , Keyboard.ups KeyUp
+        ]
---
+++ web/elm/Demos/UfoLanding.elm
@@ -0,0 +1,103 @@
+-- UFO Landing
+-- Uses SVG to display a UFO image and the ground. Animates the UFO moving from
+-- the top of the screen down to the ground area.
+
+
+module Main exposing (..)
+
+import Html.App as App
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Time exposing (..)
+
+
+-- MAIN
+
+
+main : Program Never
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time
+
+
+update : Msg -> Model -> ( Int, Cmd Msg )
+update msg model =
+    case msg of
+        Tick newTime ->
+            ( model + 1, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        position =
+            if model > 325 then
+                toString 325
+            else
+                toString model
+    in
+        Svg.svg
+            [ Svg.Attributes.version "1.1"
+            , Svg.Attributes.width "840"
+            , Svg.Attributes.height "525"
+            , Svg.Attributes.viewBox ("0 0 840 525")
+            , Html.Attributes.style [ ( "background-color", "black" ) ]
+            ]
+            [ Svg.rect
+                [ Svg.Attributes.class "ground"
+                , Svg.Attributes.fill "darkgreen"
+                , Svg.Attributes.x "0"
+                , Svg.Attributes.y "425"
+                , Svg.Attributes.width "840"
+                , Svg.Attributes.height "100"
+                ]
+                []
+            , Svg.image
+                [ Svg.Attributes.xlinkHref "img/ufo.png"
+                , Svg.Attributes.x "370"
+                , Svg.Attributes.y position
+                , Svg.Attributes.width "100"
+                , Svg.Attributes.height "100"
+                ]
+                []
+            ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every millisecond Tick
