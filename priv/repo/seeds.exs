cbfbcaceb02cbaa6f3dd4c775dc227aa96aaacf3
Bijan <bijanbwb@gmail.com>
2016-11-14 17:01:24 -0500

    Add ListCreator demo from live coding session

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "HelloWorld", liveDemoUrl: "/demos/HelloWorld.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/HelloWorld.elm"})
+ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "ListCreator", liveDemoUrl: "/demos/ListCreator.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/ListCreator.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Counter", liveDemoUrl: "/demos/Counter.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/Counter.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "CalorieCounter", liveDemoUrl: "/demos/CalorieCounter.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/CalorieCounter.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "GuessingGame", liveDemoUrl: "/demos/GuessingGame.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/GuessingGame.elm"})
@@ -17,6 +18,7 @@ ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "RocketLaunch", liveDemoUrl: "/de
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "UfoLanding", liveDemoUrl: "/demos/UfoLanding.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/UfoLanding.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Clock", liveDemoUrl: "/demos/Clock.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/Clock.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Train", liveDemoUrl: "/demos/Train.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/Train.elm"})
+ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "ElmMario", liveDemoUrl: "/demos/ElmMario.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/ElmMario.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "ElmOrlando", liveDemoUrl: "/demos/ElmOrlando.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/ElmOrlando.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "RandomDice", liveDemoUrl: "/demos/RandomDice.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/RandomDice.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "GifFetch", liveDemoUrl: "/demos/GifFetch.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/GifFetch.elm"})
@@ -27,4 +29,3 @@ ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "GraphicsShapes", liveDemoUrl: "/
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "GraphicsStamps", liveDemoUrl: "/demos/GraphicsStamps.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/GraphicsStamps.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "ElmojiTranslator", liveDemoUrl: "/demos/ElmojiTranslator.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/ElmojiTranslator.elm"})
 ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "Routing", liveDemoUrl: "/demos/Routing.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/Routing.elm"})
-ElmOrlando.Repo.insert!(%ElmOrlando.Demo{name: "ElmMario", liveDemoUrl: "/demos/ElmMario.html", sourceCodeUrl: "https://github.com/ElmOrlando/ElmOrlando/blob/master/web/elm/Demos/ElmMario.elm"})
---
+++ web/elm/Demos/ListCreator.elm
@@ -0,0 +1,149 @@
+{-
+   **ListCreator Demo**
+
+   This file contains the results of a live collaborative coding session we had
+   for our November meeting of Elm Orlando. We set out to create a simple list
+   application completely from scratch and managed to get a toggle interaction
+   working too.
+
+   Here is the checklist that we used as a guide to start the project, but we
+   also managed to keep things light and didn't follow it very closely during
+   the live session since everyone did a fantastic job of working through the
+   process of solving the problem.
+
+   # Building a List with Elm
+
+   ## Configuration
+
+   - [x] Create an `elm` folder and `cd` into it.
+   - [x] Install packages.
+   - [x] Create initial Elm source file.
+   - [x] Open folder in Atom.
+
+   ## Setup
+
+   - [x] Import `Html` package.
+   - [x] Use `main` function to display `"Hello World"` text.
+   - [x] Start `elm-reactor` and open browser.
+   - [x] Add comments to delineate MAIN, MODEL, UPDATE, and VIEW.
+
+   ## Model
+
+   - [x] Create type alias for `Model` with record containing a list of strings.
+   - [x] Create initial `model` with empty list `[]`.
+   - [x] Add a couple of strings to the list for us to work with.
+   - [x] Add type annotations.
+
+   ## Update
+
+   - [x] Create `Msg` type with initial `NoOp`.
+   - [x] Explain `update` with `msg` and `model` parameters.
+   - [x] Add type annotation for `update` function.
+   - [x] Create `update` with `case` and `NoOp`.
+
+   ## View
+
+   - [x] Add type annotation for `view` function.
+   - [x] Create `view` with initial empty `div` element.
+   - [x] Inside the `div`, create `ul` and hard-code a couple of `li` elements.
+
+   ## Html.App
+
+   - [x] Wire everything together by importing `Html.App`.
+   - [x] Update `main` with `App.beginnerProgram`.
+
+   ## View Refactoring
+
+   - [x] Extract hardcoded list to `list` and `listItem` functions.
+   - [x] Use `List.map` to render each `listItem` in the `model`'s list.
+
+   ## Interactivity
+
+   - [x] Add a `button` element to toggle list.
+   - [x] Import `Html.Events` and `onClick`.
+   - [x] Update `Msg` and `update` with `ShowList`.
+   - [x] Update `Model` and `model` with a `showList` of type `Bool`.
+   - [x] Update the `ShowList` message to change the `showList` value to `True`.
+   - [x] Add a conditional to our `list` function.
+   - [x] If `model.showList` is `True`, then display the list.
+   - [x] Otherwise, show an empty list with `ul [] []`.
+   - [x] Try it out in the browser!
+
+-}
+
+
+module ListCreator exposing (..)
+
+import Html.App as App
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+-- MODEL
+
+
+type alias Model =
+    { showList : Bool
+    , list : List String
+    }
+
+
+model : Model
+model =
+    { showList = False
+    , list = [ "one", "two", "three" ]
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | DisplayToggle
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+        DisplayToggle ->
+            { model | showList = not model.showList }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view { list, showList } =
+    div []
+        [ button [ onClick DisplayToggle ] [ text "Display List" ]
+        , if showList then
+            ol
+                []
+                (List.map viewListItem list)
+          else
+            ol [] []
+        ]
+
+
+viewListItem : String -> Html a
+viewListItem string =
+    li [] [ text string ]
+
+
+
+-- MAIN
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
