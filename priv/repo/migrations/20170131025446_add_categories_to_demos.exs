b60a4c01f11354f9c4e49a43551cb5c7bbfd4a94
Bijan <bijanbwb@gmail.com>
2017-01-30 21:57:01 -0500

    Fix table name in migration

--- priv/repo/migrations/20170131025446_add_categories_to_demos.exs
+++ priv/repo/migrations/20170131025446_add_categories_to_demos.exs
@@ -2,7 +2,7 @@ defmodule ElmOrlando.Repo.Migrations.AddCategoriesToDemos do
   use Ecto.Migration
 
   def change do
-    alter table(:users) do
+    alter table(:demos) do
       add :category, :string
     end
   end
